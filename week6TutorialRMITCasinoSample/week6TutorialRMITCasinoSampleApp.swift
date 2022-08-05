c9564a554cfd3e209398365b0954a6de2021554c
Tadaki <tadaki741@gmail.com>
2022-08-05 13:21:50 +0700

    Initial Commit

---
+++ week6TutorialRMITCasinoSample/week6TutorialRMITCasinoSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  week6TutorialRMITCasinoSampleApp.swift
+//  week6TutorialRMITCasinoSample
+//
+//  Created by Duong Vo Dai on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct week6TutorialRMITCasinoSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
